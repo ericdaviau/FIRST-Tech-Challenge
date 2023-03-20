65b7452f539b383031ec7cb2a2082f873254fec5
ericdaviau <onetime@daviau.com>
2023-03-20 15:42:07 -0400

    First Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class BasicAuto extends LinearOpMode {
+public class git branchBasicAuto extends LinearOpMode {
 
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
